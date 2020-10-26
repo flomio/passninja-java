b8da9c285b459602de45c0c7cac296f41855ca5a
Ioannis Mavroukakis <imavroukakis@gmail.com>
2020-10-26 09:49:40 +0200

    use Junit5 and AssertJ to drive tests

--- src/test/java/com/passninja/model/PassTest.java
+++ src/test/java/com/passninja/model/PassTest.java
@@ -1,49 +1,53 @@
 package com.passninja.model;
 
-import com.passninja.BaseTest;
 import com.passninja.Passninja;
 import com.passninja.exception.AuthenticationException;
 import com.passninja.net.PassninjaResponse;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsInstanceOf.instanceOf;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
-public class PassTest extends BaseTest {
+public class PassTest {
+
+    @BeforeEach
+    void setup() {
+        Passninja.init("69d78c80-1e8e-482b-8f17-15027f28facc", "zIyRpESRkD7Ny1jxiNKYs3qHthW0EtSm9hKWL8G5");
+    }
+
 
     @Test
-    public void createPass() throws Exception {
-        super.beforeClass();
+    @DisplayName("Should create a new pass")
+    public void pass_is_created_successfully() throws Exception {
         PassninjaResponse<Pass> response = Pass.create("Name.a", new HashMap<>());
-
         Pass pass = response.getResponseBody();
-
-        assertEquals(200, response.getResponseCode());
-        assertThat(pass.getUrls(), instanceOf(Map.class));
+        assertThat(response.getResponseCode()).as("check successful response").isEqualTo(200);
+        assertThat(pass.getUrls()).as("response contains urls").containsKeys("landing", "google", "apple");
     }
 
     @Test
-    public void getPass() throws Exception {
-        super.beforeClass();
+    @DisplayName("Should retrieve a pass by serial number")
+    public void pass_can_be_retrieved_by_serial_number() throws Exception {
         PassninjaResponse<Pass> response = Pass.create("Name.a", new HashMap<>());
 
-        assertEquals(200, response.getResponseCode());
-        PassninjaResponse<Pass> value = Pass.get("Name.a", response.getResponseBody().getSerialNumber());
-        assertEquals(response.getResponseBody().getSerialNumber(), value.getResponseBody().getSerialNumber());
+        assertThat(response.getResponseCode()).as("check successful response").isEqualTo(200);
+        Pass responseBody = response.getResponseBody();
+        PassninjaResponse<Pass> value = Pass.get("Name.a", responseBody.getSerialNumber());
+        assertThat(responseBody.getSerialNumber()).isEqualTo(value.getResponseBody().getSerialNumber());
     }
 
     @Test
+    @DisplayName("Should put new details into a pass")
     public void putPass() throws Exception {
-        super.beforeClass();
         PassninjaResponse<Pass> response = Pass.create("Name.a", new HashMap<>());
 
-        assertEquals(200, response.getResponseCode());
+        assertThat(response.getResponseCode()).as("check successful response").isEqualTo(200);
         Map<String, Object> pass = new HashMap<>();
         pass.put("logoText", "Put Example Loyalty");
         pass.put("organizationName", "Put my org");
@@ -53,41 +57,38 @@ public class PassTest extends BaseTest {
         pass.put("specialOffer", "Put Free Drinks at 4:30PM!");
         pass.put("loyaltyLevel", "put level one");
         pass.put("barcode", "www.put.com");
-        PassninjaResponse<Pass> value = Pass.put("Name.a", response.getResponseBody().getSerialNumber(), pass);
-        assertEquals(response.getResponseBody().getSerialNumber(), value.getResponseBody().getSerialNumber());
-        assertEquals("Put Free Drinks at 4:30PM!", value.getResponseBody().getPass().get("specialOffer"));
+        Pass originalResponseBody = response.getResponseBody();
+        Pass newPassResponseBody = Pass.put("Name.a", originalResponseBody.getSerialNumber(), pass).getResponseBody();
+        assertThat(originalResponseBody.getSerialNumber()).isEqualTo(newPassResponseBody.getSerialNumber());
+        assertThat(newPassResponseBody.getPass()).containsAllEntriesOf(pass);
     }
 
     @Test
+    @DisplayName("Should delete a pass")
     public void deletePass() throws Exception {
-        super.beforeClass();
         PassninjaResponse<Pass> response = Pass.create("Name.a", new HashMap<>());
-        assertEquals(200, response.getResponseCode());
-        Pass.delete("Name.a", response.getResponseBody().getSerialNumber());
-        PassninjaResponse<Pass> value = Pass.get("Name.a", response.getResponseBody().getSerialNumber());
-        assertEquals(response.getResponseBody().getSerialNumber(), value.getResponseBody().getSerialNumber());
+        assertThat(response.getResponseCode()).as("check successful response").isEqualTo(200);
+        Pass responseBody = response.getResponseBody();
+        Pass.delete("Name.a", responseBody.getSerialNumber());
+        PassninjaResponse<Pass> value = Pass.get("Name.a", responseBody.getSerialNumber());
+        assertThat(responseBody.getSerialNumber()).isEqualTo(value.getResponseBody().getSerialNumber());
     }
 
     @Test
+    @DisplayName("Should force delete a pass")
     public void deleteForcePass() throws Exception {
-        super.beforeClass();
         PassninjaResponse<Pass> response = Pass.create("Name.a", new HashMap<>());
-        assertEquals(200, response.getResponseCode());
-        Pass.deleteForce("Name.a", response.getResponseBody().getSerialNumber());
-        PassninjaResponse<Pass> value = Pass.get("Name.a", response.getResponseBody().getSerialNumber());
-        assertEquals(response.getResponseBody().getSerialNumber(), value.getResponseBody().getSerialNumber());
+        assertThat(response.getResponseCode()).as("check successful response").isEqualTo(200);
+        Pass responseBody = response.getResponseBody();
+        Pass.deleteForce("Name.a", responseBody.getSerialNumber());
+        PassninjaResponse<Pass> value = Pass.get("Name.a", responseBody.getSerialNumber());
+        assertThat(responseBody.getSerialNumber()).isEqualTo(value.getResponseBody().getSerialNumber());
     }
 
     @Test
-    public void notValidKey() throws Exception {
+    @DisplayName("Should fail when supplied with invalid pass credentials")
+    public void notValidKey() {
         Passninja.init("", "");
-        try {
-            Pass.create("Name.a", new HashMap<>());
-            fail("AuthenticationException is expected");
-        } catch (AuthenticationException expected) {
-            expected.getStatusCode();
-            expected.getMessage();
-        }
+        Assertions.assertThrows(AuthenticationException.class, () -> Pass.create("Name.a", new HashMap<>()));
     }
-
 }
